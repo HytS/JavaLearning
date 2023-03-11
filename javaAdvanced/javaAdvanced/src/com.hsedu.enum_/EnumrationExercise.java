31bb5fc8e1b1a24693ed02252ec4f81eda919bba
houshuai <m17856943636@163.com>
2023-03-11 21:23:33 +0800

    更新代码

---
+++ javaAdvanced/javaAdvanced/src/com.hsedu.enum_/EnumrationExercise.java
@@ -0,0 +1,24 @@
+public class EnumrationExercise {
+    public static void main(String[] args) {
+        Week[] weeKs = Week.values();
+        for (Week week : weeKs) {
+            System.out.println(week);
+        }
+    }
+}
+
+enum Week {
+    MONDAY("星期一"), TUSEDAY("星期二"), WEDNESDAY("星期三"), THURSDAY("星期四"),
+    FRIDAY("星期五"), SATURDAY("星期六"), SUNDAY("星期天");
+
+    private String name;
+
+    private Week(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString(String name) {
+        return name;
+    }
+}
