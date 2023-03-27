95434d835cf0667ccc07a8f9065eac6d74cf2c59
houshuai <m17856943636@163.com>
2023-03-27 16:30:54 +0800

    更新

--- javaAdvanced/javaAdvanced/src/com/hsedu/generic/CustomMethodGeneric.java
+++ javaAdvanced/javaAdvanced/src/com/hsedu/generic/CustomMethodGeneric.java
@@ -1,2 +1,21 @@
-package com.hsedu.generic;public class CustomMethodGeneric {
+package com.hsedu.generic;
+
+public class CustomMethodGeneric {
+    public static void main(String[] args) {
+        AGen a=new AGen();
+        a.sayhi("tom",12);
+        System.out.println(a);
+    }
+}
+class AGen{
+    public <T,R> void sayhi(T t,R r){
+
+    }
+
+}
+
+class BGen<U,M>{
+    public <U,M> void say(U u,M m){
+
+    }
 }
