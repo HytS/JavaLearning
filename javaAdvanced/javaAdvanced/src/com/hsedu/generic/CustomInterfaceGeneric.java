af567572aea8e26cbea974307c4cb90df0c26830
houshuai <m17856943636@163.com>
2023-03-27 16:30:51 +0800

    更新

--- javaAdvanced/javaAdvanced/src/com/hsedu/generic/CustomInterfaceGeneric.java
+++ javaAdvanced/javaAdvanced/src/com/hsedu/generic/CustomInterfaceGeneric.java
@@ -1,2 +1,43 @@
-package com.hsedu.generic;public class CustomInterfaceGeneric {
+package com.hsedu.generic;
+
+public class CustomInterfaceGeneric {
+    public static void main(String[] args) {
+
+    }
+}
+
+class AA implements IA{
+
+    @Override
+    public Double get(String s) {
+        return null;
+    }
+}
+
+class BB implements IF<Integer,Float>{
+
+    @Override
+    public Float get(Integer integer) {
+        return null;
+    }
+}
+
+class CC implements IF{
+
+    @Override
+    public Object get(Object o) {
+        return null;
+    }
+}
+
+interface IA extends IF<String,Double>{
+
+
+
+}
+
+interface IF<U,R>{
+    int n=10;
+    R get(U u);
+
 }
