4f61db177294cc65be962c1a07dbbf1c06c6fe21
houshuai <m17856943636@163.com>
2023-03-28 18:45:42 +0800

    更新

--- javaAdvanced/javaAdvanced/src/com/hsedu/generic/generic03.java
+++ javaAdvanced/javaAdvanced/src/com/hsedu/generic/generic03.java
@@ -1,2 +1,23 @@
-package com.hsedu.generic;public class generic03 {
+package com.hsedu.generic;
+
+public class generic03 {
+    public static void main(String[] args) {
+        //E的具体数据类型在定义Person对象时指定，即在编译期间，就确定E是什么类型
+        Person<String> person = new Person<String>("adis");
+        System.out.println(person);
+        Person<Integer> person1 = new Person<Integer>(123);
+        System.out.println(person1);
+    }
+}
+class Person<E>{
+    //E表示s的类型，该数据类型在定义Person对象的时候指定，即在编译期间，就确定E是什么类型
+  E s;
+  //参数
+    public Person(E e) {
+        this.s=e;
+    }
+    //返回类型
+    public E say(){
+        return s;
+    }
 }
--- javaAdvanced/javaAdvanced/src/com/hsedu/generic/genericDetail.java
+++ javaAdvanced/javaAdvanced/src/com/hsedu/generic/genericDetail.java
@@ -1,2 +1,26 @@
-package com.hsedu.generic;public class genericDetail {
+package com.hsedu.generic;
+
+import java.util.ArrayList;
+
+public class genericDetail {
+    public static void main(String[] args) {
+        Pig<A> aPig = new Pig<A>(new A());
+        Pig<A> aPig1 = new Pig<A>(new B());
+
+        //推荐这种写法
+        ArrayList<Object> objects = new ArrayList<>();
+        //默认是Object
+        ArrayList object1 = new ArrayList();
+
+    }
+}
+class A{}
+class B extends A{}
+
+class Pig<E>{
+    E e;
+
+    public Pig(E e) {
+        this.e = e;
+    }
 }
