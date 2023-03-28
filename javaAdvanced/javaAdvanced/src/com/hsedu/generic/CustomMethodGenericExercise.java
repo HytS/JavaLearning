a7d791a4de86c7fa3fd36ddd0c76a357ca4d316c
houshuai <m17856943636@163.com>
2023-03-28 18:45:32 +0800

    更新

--- javaAdvanced/javaAdvanced/src/com/hsedu/generic/CustomMethodGenericExercise.java
+++ javaAdvanced/javaAdvanced/src/com/hsedu/generic/CustomMethodGenericExercise.java
@@ -1,2 +1,18 @@
-package com.hsedu.generic;public class CustomMethodGenericExercise {
+package com.hsedu.generic;
+
+public class CustomMethodGenericExercise {
+    public static void main(String[] args) {
+        Apple<String,Integer,Double> a=new Apple();
+        a.fly(10);
+        a.fly(new DogG());
+    }
+}
+class Apple<T,R,M>{
+    public <E> void fly(E e){
+        System.out.println(e.getClass().getSimpleName());
+    }
+
+}
+class DogG{
+
 }
--- javaAdvanced/javaAdvanced/src/com/hsedu/generic/generic02.java
+++ javaAdvanced/javaAdvanced/src/com/hsedu/generic/generic02.java
@@ -1,2 +1,49 @@
-package com.hsedu.generic;public class generic02 {
+package com.hsedu.generic;
+
+import java.util.ArrayList;
+
+public class generic02 {
+    public static void main(String[] args) {
+        ArrayList<Dog> dogs = new ArrayList<Dog>();
+        dogs.add(new Dog("tom",12));
+        dogs.add(new Dog("aom",15));
+        dogs.add(new Dog("vom",14));
+        for (Object o :dogs) {
+            Dog d=(Dog) o;
+            System.out.println(d.getName() + d.getAge());
+        }
+    }
+}
+class Dog{
+    private String name;
+    private int age;
+
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
