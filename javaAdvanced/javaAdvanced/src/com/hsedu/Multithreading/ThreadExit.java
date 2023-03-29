3f98138a93b0fb9976c8c7bccf4f0fd0ac9c50b3
houshuai <m17856943636@163.com>
2023-03-29 10:18:31 +0800

    更新代码

--- javaAdvanced/javaAdvanced/src/com/hsedu/Multithreading/ThreadExit.java
+++ javaAdvanced/javaAdvanced/src/com/hsedu/Multithreading/ThreadExit.java
@@ -1,2 +1,31 @@
-package com.hsedu.Multithreading;public class ThreadExit {
+package com.hsedu.Multithreading;
+
+public class ThreadExit {
+    public static void main(String[] args) throws InterruptedException {
+        T t = new T();
+        t.start();
+        System.out.println("main–›√ﬂ10s");
+        Thread.sleep(10*1000);
+        t.setLoop(false);
+    }
+}
+class T extends Thread{
+    private int count=0;
+    private boolean loop=true;
+
+    @Override
+    public void run() {
+        while (loop){
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("sleep"+(++count));
+        }
+    }
+
+    public void setLoop(boolean loop) {
+        this.loop = loop;
+    }
 }
