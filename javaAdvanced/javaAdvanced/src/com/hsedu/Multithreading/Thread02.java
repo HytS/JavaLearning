a54f13acf829df7e5b42c9dc1882fc17958efb2d
houshuai <m17856943636@163.com>
2023-03-29 10:15:10 +0800

    更新代码

--- javaAdvanced/javaAdvanced/src/com/hsedu/Multithreading/Thread02.java
+++ javaAdvanced/javaAdvanced/src/com/hsedu/Multithreading/Thread02.java
@@ -1,2 +1,52 @@
-package com.hsedu.Multithreading;public class Thread02 {
+package com.hsedu.Multithreading;
+
+public class Thread02 {
+    public static void main(String[] args) {
+//        Thread thread=new Thread(new Dog());
+//        thread.start();
+        Tiger tiger = new Tiger();
+        ThreadProxy threadProxy = new ThreadProxy(tiger);
+        threadProxy.start();
+    }
+}
+
+class Tiger implements Runnable{
+    @Override
+    public void run() {
+        System.out.println("老虎胶");
+    }
+}
+class ThreadProxy implements Runnable{
+    private Runnable target=null;
+
+    @Override
+    public void run() {
+        if (target!=null) target.run();//target动态绑定传进来的对象
+
+    }
+
+    public ThreadProxy(Runnable target) {
+        this.target = target;
+    }
+    public void start(){
+        start0();
+    }
+    public  void start0(){
+        run();
+    }
+}
+class Dog implements Runnable{
+    @Override
+    public void run() {
+        int count=0;
+        while (true){
+            System.out.println("汪汪"+(++count)+"name="+Thread.currentThread().getName());
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            if (count==10) break;
+        }
+    }
 }
--- javaAdvanced/javaAdvanced/src/com/hsedu/Multithreading/Thread03.java
+++ javaAdvanced/javaAdvanced/src/com/hsedu/Multithreading/Thread03.java
@@ -1,2 +1,43 @@
-package com.hsedu.Multithreading;public class Thread03 {
+package com.hsedu.Multithreading;
+
+public class Thread03 {
+    public static void main(String[] args) {
+        Thread thread=new Thread(new AM());
+        thread.start();
+    }
+}
+
+class AM implements Runnable{
+    @Override
+    public void run() {
+        int count=0;
+       while (true){
+           System.out.println("hi");
+           try {
+               Thread.sleep(1000);
+           } catch (InterruptedException e) {
+               throw new RuntimeException(e);
+           }
+           count++;
+           if (count==4) break;
+       }
+
+    }
+}
+class BM implements Runnable{
+    @Override
+    public void run() {
+        int count=0;
+        while (true){
+            System.out.println("helloworld");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            count++;
+            if (count==10) break;
+        }
+
+    }
 }
