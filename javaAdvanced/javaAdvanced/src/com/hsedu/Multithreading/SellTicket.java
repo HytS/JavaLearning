f821febcdcf2eaa2cfd0001e35c00517bc497be1
houshuai <m17856943636@163.com>
2023-03-29 10:14:27 +0800

    更新代码

--- javaAdvanced/javaAdvanced/src/com/hsedu/Multithreading/SellTicket.java
+++ javaAdvanced/javaAdvanced/src/com/hsedu/Multithreading/SellTicket.java
@@ -1,2 +1,52 @@
-package com.hsedu.Multithreading;public class SellTicket {
+package com.hsedu.Multithreading;
+
+public class SellTicket {
+    public static void main(String[] args) {
+//        SellTicket01 sellTicket01 = new SellTicket01();
+//        SellTicket01 sellTicket02 = new SellTicket01();
+//        SellTicket01 sellTicket03 = new SellTicket01();
+        SellTicket02 sellTicket01 = new SellTicket02();
+        new Thread(sellTicket01).start();
+        new Thread(sellTicket01).start();
+        new Thread(sellTicket01).start();
+    }
+}
+class SellTicket01 extends Thread{
+    private static int ticketNum=100;
+
+    @Override
+    public void run() {
+        while (true){
+            if (ticketNum<=0){
+                System.out.println("ticket is 0");
+                break;
+            }
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("´°¿Ú"+Thread.currentThread().getName()+"Ê£ÓàÆ±Êý"+(--ticketNum));
+        }
+    }
+}
+
+class SellTicket02 implements Runnable{
+    private  int ticketNum=10;
+
+    @Override
+    public void run() {
+        while (true){
+            if (ticketNum==0){
+                System.out.println("ticket is 0");
+                break;
+            }
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("´°¿Ú"+Thread.currentThread().getName()+"Ê£ÓàÆ±Êý"+(--ticketNum));
+        }
+    }
 }
