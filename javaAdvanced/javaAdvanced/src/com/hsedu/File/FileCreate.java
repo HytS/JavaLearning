7457f2e990c6ec385c62a16a7199fadc6c8170ae
houshuai <m17856943636@163.com>
2023-03-30 17:05:20 +0800

    更新

--- javaAdvanced/javaAdvanced/src/com/hsedu/File/FileCreate.java
+++ javaAdvanced/javaAdvanced/src/com/hsedu/File/FileCreate.java
@@ -1,2 +1,52 @@
-package com.hsedu.File;public class FileCreate {
+package com.hsedu.File;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileCreate {
+    public static void main(String[] args) {
+
+    }
+
+    @Test
+    public void create01(){
+        String filePath="e:\\new1.txt";
+        File file=new File(filePath);
+
+        try {
+            file.createNewFile();
+            System.out.println("文件创建成功");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Test
+    public void create02(){
+        File parFile= new File("e:\\");
+        String fileName="new2.txt";
+        File file = new File(parFile, fileName);
+        try {
+            file.createNewFile();
+            System.out.println("new2创建成功");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Test
+    public void create03(){
+        String filepath= "e:\\";
+        String fileName="new3.txt";
+        File file = new File(filepath, fileName);
+        try {
+            file.createNewFile();
+            System.out.println("new3创建成功");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
--- javaAdvanced/javaAdvanced/src/com/hsedu/Multithreading/ThreadMethod03.java
+++ javaAdvanced/javaAdvanced/src/com/hsedu/Multithreading/ThreadMethod03.java
@@ -1,2 +1,29 @@
-package com.hsedu.Multithreading;public class ThreadMethod03 {
+package com.hsedu.Multithreading;
+
+public class ThreadMethod03 {
+    public static void main(String[] args) throws InterruptedException {
+        MyThread myThread = new MyThread();
+        myThread.setDaemon(true);
+        myThread.start();
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("hard working");
+            Thread.sleep(1000);
+        }
+    }
+}
+class MyThread extends Thread{
+    @Override
+    public void run() {
+        while (true){
+            for(;;){
+                System.out.println("happy chatting");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
 }
