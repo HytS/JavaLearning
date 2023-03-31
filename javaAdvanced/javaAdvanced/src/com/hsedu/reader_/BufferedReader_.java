b8e52e68eac9ca9d190ea244d294e3c45d4e0ea1
houshuai <m17856943636@163.com>
2023-03-31 10:27:57 +0800

    更新

---
+++ javaAdvanced/javaAdvanced/src/com/hsedu/reader_/BufferedReader_.java
@@ -0,0 +1,16 @@
+package com.hsedu.reader_;
+
+import java.io.FileReader;
+
+public class BufferedReader {
+
+
+    public static void main(String[] args) throws Exception {
+        String filePath="e:\\a.java";
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath));
+        String line;
+        while ((line=bufferedReader.re)!=null){
+
+        }
+    }
+}
---
+++ javaAdvanced/javaAdvanced/src/com/hsedu/writer_/BufferedCopy.java
@@ -0,0 +1,2 @@
+package com.hsedu.writer_;public class BufferedCopy {
+}
