31c9a8c4c1f5c61309e178925d2ef0159c071466
houshuai <m17856943636@163.com>
2023-04-01 18:47:46 +0800

    更新

--- javaAdvanced/javaAdvanced/src/com/hsedu/reader_/BufferedReader_.java
+++ javaAdvanced/javaAdvanced/src/com/hsedu/reader_/BufferedReader_.java
@@ -1,16 +1,19 @@
 package com.hsedu.reader_;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 
-public class BufferedReader {
+public class BufferedReader_ {
 
 
     public static void main(String[] args) throws Exception {
-        String filePath="e:\\a.java";
+        String filePath="e:\\a.txt";
         BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath));
         String line;
-        while ((line=bufferedReader.re)!=null){
-
+        while ((line=bufferedReader.readLine())!=null){
+            System.out.println(line);
         }
+        //关闭流，只需要关闭BufferedReader
+        bufferedReader.close();
     }
 }
--- javaAdvanced/javaAdvanced/src/com/hsedu/socket_/socketTCP01.java
+++ javaAdvanced/javaAdvanced/src/com/hsedu/socket_/socketTCP01.java
@@ -1,2 +1,29 @@
-package com.hsedu.socket_;public class socketTCP01 {
+package com.hsedu.socket_;
+
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class socketTCP01 {
+    public static void main(String[] args) throws Exception{
+        //要求在本机没有其他服务在监听9999
+        ServerSocket serverSocket = new ServerSocket(9999);
+        System.out.println("服务端在9999端口等待连接");
+        //当没有客户端连接9999端口时，程序会阻塞
+        //有客户端连接，会返回socket对象，程序继续
+        Socket socket = serverSocket.accept();
+        System.out.println("socket="+socket.getClass());
+
+        InputStream inputStream = socket.getInputStream();
+        //IO读取
+        int readLen=0;
+        byte[] bytes=new byte[1024];
+        while ((readLen=inputStream.read(bytes))!=-1){
+            System.out.println(new String(bytes,0,readLen));
+        }
+
+        inputStream.close();
+        socket.close();
+        serverSocket.close();
+    }
 }
