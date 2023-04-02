cde3e77c079c012e7d13e679afe35b20cfa3b319
houshuai <m17856943636@163.com>
2023-04-02 08:08:53 +0800

    更新

--- javaAdvanced/javaAdvanced/src/com/hsedu/Cat.java
+++ javaAdvanced/javaAdvanced/src/com/hsedu/Cat.java
@@ -1,2 +1,13 @@
-package com.hsedu;public class Cat {
+package com.hsedu;
+
+public class Cat {
+    private  String name;
+    public  int age=199;
+    public Cat(){}
+    public Cat(String name){
+        this.name=name;
+    }
+    public void hi(){
+        System.out.println("hi");
+    }
 }
