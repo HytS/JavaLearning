dcac73b3b11c6f02052273b521031e82ecab57bf
houshuai <m17856943636@163.com>
2023-04-02 11:18:30 +0800

    更新

---
+++ javaAdvanced/javaAdvanced/src/com/hsedu/Car.java
@@ -0,0 +1,12 @@
+public class Car {
+    private String brand="bmw";
+    public int no=1024;
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", no=" + no +
+                '}';
+    }
+}
