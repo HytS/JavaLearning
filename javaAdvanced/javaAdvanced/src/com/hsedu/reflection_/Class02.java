246917b0d43d4661d1589f7e5d3b8718b30a7eff
houshuai <m17856943636@163.com>
2023-04-02 16:00:25 +0800

    更新

--- javaAdvanced/javaAdvanced/src/com/hsedu/reflection_/Class02.java
+++ javaAdvanced/javaAdvanced/src/com/hsedu/reflection_/Class02.java
@@ -1,2 +1,31 @@
-package com.hsedu.reflection_;public class Class02 {
+package com.hsedu.reflection_;
+
+import java.lang.reflect.Field;
+
+public class Class02 {
+    public static void main(String[] args) throws Exception{
+        Class<?> cls = Class.forName("com.hsedu.Car");
+
+        System.out.println(cls);//显示cls对象，是那个类的Class对象
+        System.out.println(cls.getClasses());//输出cls运行类型
+        //获取包名
+        System.out.println(cls.getPackage().getName());
+        //得到全类名
+        System.out.println(cls.getName());
+        //创建对象实例
+        Object o = cls.newInstance();
+        System.out.println(o);
+
+        Field no = cls.getField("no");
+        System.out.println(no);
+        System.out.println(no.get(o));
+        //通过反射赋值
+        no.set(o,10000);
+
+        System.out.println("=====所有字段属性=====");
+        Field[] fields = cls.getFields();
+        for (Field f : fields) {
+            System.out.println(f.getName());
+        }
+    }
 }
--- javaAdvanced/javaAdvanced/src/com/hsedu/reflection_/Reflection01.java
+++ javaAdvanced/javaAdvanced/src/com/hsedu/reflection_/Reflection01.java
@@ -1,2 +1,32 @@
-package com.hsedu.reflection_;public class Reflection01 {
+package com.hsedu.reflection_;
+
+import java.io.FileInputStream;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Properties;
+
+public class Reflection01 {
+    public static void main(String[] args) throws Exception{
+        Properties properties = new Properties();
+        properties.load(new FileInputStream("D:\\gitLearning\\JavaLearning\\javaAdvanced\\javaAdvanced\\src\\re.properties"));
+        String classfullpath = properties.get("classfullpath").toString();
+        String methodName = properties.get("method").toString();
+
+        Class<?> cls = Class.forName(classfullpath);
+        Object o = cls.newInstance();
+        Method method1 = cls.getMethod(methodName);
+        method1.invoke(o);
+
+        //getField 不能得到私有的属性
+        Field nameField = cls.getField("age");
+        System.out.println(nameField.get(o));//反射：成员变量对象.get(对象)
+
+        Constructor<?> constructor = cls.getConstructor();
+        System.out.println(constructor);
+
+        Constructor<?> constructor1 = cls.getConstructor(String.class);
+        System.out.println(constructor1);
+
+    }
 }
