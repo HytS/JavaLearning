196a2f43ebbf22790ce1f4468b40642416252d24
houshuai <m17856943636@163.com>
2023-04-02 16:00:31 +0800

    更新

--- javaAdvanced/javaAdvanced/src/com/hsedu/reflection_/Reflection02.java
+++ javaAdvanced/javaAdvanced/src/com/hsedu/reflection_/Reflection02.java
@@ -1,2 +1,34 @@
-package com.hsedu.reflection_;public class Reflection02 {
+package com.hsedu.reflection_;
+
+import com.hsedu.Cat;
+
+import java.lang.reflect.Method;
+
+public class Reflection02 {
+    public static void main(String[] args) throws Exception{
+        m1();
+        m2();
+    }
+    public static void m1(){
+        Cat cat = new Cat();
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 90000000; i++) {
+            cat.hi();
+        }
+        long end=System.currentTimeMillis();
+        System.out.println("m1()用时="+(end-start));
+    }
+
+    public static void m2() throws Exception{
+        Class<?> cls = Class.forName("com.hsedu.Cat");
+        Object o = cls.newInstance();
+        Method hi = cls.getMethod("hi");
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 90000000; i++) {
+            hi.invoke(o);
+        }
+        long end=System.currentTimeMillis();
+        System.out.println("m2()用时="+(end-start));
+    }
 }
--- javaAdvanced/javaAdvanced/src/com/hsedu/reflection_/ReflectionCreateInstance.java
+++ javaAdvanced/javaAdvanced/src/com/hsedu/reflection_/ReflectionCreateInstance.java
@@ -1,2 +1,47 @@
-package com.hsedu.reflection_;public class ReflectionCreateInstance {
+package com.hsedu.reflection_;
+
+import java.lang.reflect.Constructor;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class ReflectionCreateInstance {
+    public static void main(String[] args) throws Exception{
+        Class<?> aClass = Class.forName("com.hsedu.reflection_.ReflectionCreateInstance");
+        Object o = aClass.newInstance();
+        System.out.println(o);
+
+        Constructor<?> constructor = aClass.getConstructor(String.class);
+        Object o1 = constructor.newInstance("hs");
+        System.out.println(o1);
+
+//        Constructor<?> declaredConstructor = aClass.getDeclaredConstructor(int.class,String.class);
+//
+//        declaredConstructor.setAccessible(true);
+//        Object o2 = declaredConstructor.newInstance(100,"hs");
+//        System.out.println(o2);
+
+    }
+}
+
+class User{
+    private int age=10;
+    private String name="ss";
+
+    public User(){}
+    public User(String name){
+        this.name=name;
+    }
+    private User(int age,String name){
+        this.name=name;
+        this.age=age;
+    }
+    @Override
+    public String toString() {
+        return "User{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
