8bc8275d147547302a2933ab8ade6f3a3dc66639
houshuai <m17856943636@163.com>
2023-04-02 11:18:44 +0800

    更新

--- javaAdvanced/javaAdvanced/src/com/hsedu/reflection_/AllTypeClass.java
+++ javaAdvanced/javaAdvanced/src/com/hsedu/reflection_/AllTypeClass.java
@@ -1,2 +1,27 @@
-package com.hsedu.reflection_;public class AllTypeClass {
+package com.hsedu.reflection_;
+
+import java.io.Serializable;
+
+public class AllTypeClass {
+    public static void main(String[] args) {
+        Class<String> cls1 = String.class;//外部类
+        Class<Serializable> cls2 = Serializable.class;
+        Class<float[]> cls3 = float[].class;
+        Class<float[][]> cls4 = float[][].class;
+        Class<Thread.State> cls5 = Thread.State.class;//枚举
+        Class<Long> cls6 = long.class;
+        Class<Void> cls7 = void.class;
+        Class<Class> cls8 = Class.class;
+        System.out.println(cls1);
+        System.out.println(cls2);
+        System.out.println(cls3);
+        System.out.println(cls4);
+        System.out.println(cls5);
+        System.out.println(cls6);
+        System.out.println(cls7);
+        System.out.println(cls8);
+
+
+
+    }
 }
