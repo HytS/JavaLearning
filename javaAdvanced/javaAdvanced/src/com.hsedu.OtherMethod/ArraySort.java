a78eaa435f227629d625f59a31e2ef4ad1c0d625
houshuai <m17856943636@163.com>
2023-04-03 09:08:08 +0800

    更新

--- javaAdvanced/javaAdvanced/src/com.hsedu.OtherMethod/ArraySort.java
+++ javaAdvanced/javaAdvanced/src/com.hsedu.OtherMethod/ArraySort.java
@@ -1,3 +1,5 @@
+package com.hsedu.OtherMethod;
+
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Comparator;
@@ -5,13 +7,18 @@ import java.util.Comparator;
 public class ArraySort {
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3 };
-        Arrays.sort(arr, new Comparator<T>() {
-            @Override
-            public int compare(T o1, T o2) {
-                Integer i1 = (Integer) o1;
-                Integer i2 = (Integer) o2;
-                return i1 - i2;
-            }
-        });
+
+        //???
+//        Arrays.sort(arr, new Comparator() {
+//            @Override
+//            public int compare(Object o1, Object o2) {
+//                Integer i1 = (Integer) o1;
+//                Integer i2 = (Integer) o2;
+//                return i1 - i2;
+//            }
+//        });
+
+
+
     }
 }
--- javaAdvanced/javaAdvanced/src/com.hsedu.map/HashMapSource.java
+++ javaAdvanced/javaAdvanced/src/com.hsedu.map/HashMapSource.java
@@ -1,2 +1,32 @@
-package com.hsedu.map;public class HashMapSource {
+package com.hsedu.map;
+
+import java.util.HashMap;
+
+public class HashMapSource {
+    public static void main(String[] args) {
+        HashMap hashMap = new HashMap();
+        for (int i = 1; i <= 12; i++) {
+            hashMap.put(new A(i),"hello");
+        }
+        System.out.println("hashMap="+hashMap);
+    }
+}
+
+class A{
+    public  int num;
+    public A(int num){
+        this.num=num;
+    }
+
+    @Override
+    public int hashCode() {
+        return 100;
+    }
+
+    @Override
+    public String toString() {
+        return "\nA{" +
+                "num=" + num +
+                '}';
+    }
 }
