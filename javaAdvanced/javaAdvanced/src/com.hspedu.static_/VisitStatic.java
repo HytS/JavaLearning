4c72fc8abdd7ac04f281f17b3f6ceb5f4c2acee8
houshuai <m17856943636@163.com>
2023-04-05 06:43:55 +0800

    更新

--- javaAdvanced/javaAdvanced/src/com.hspedu.static_/VisitStatic.java
+++ javaAdvanced/javaAdvanced/src/com.hspedu.static_/VisitStatic.java
@@ -1,21 +1,21 @@
-
-package com.hspedu.static_;
-
-public class VisitStatic {
-    public static void main(String[] args) {
-        System.out.println(A.name);
-        A a = new A();
-        System.out.println(a.name);        
-    }
-}
-
-
-/**
- * A
- */
-public class A {
-
-    public static String name="hsedu";
-
-
-}
+//
+//package com.hspedu.static_;
+//
+//public class VisitStatic {
+//    public static void main(String[] args) {
+//        System.out.println(A.name);
+//        A a = new A();
+//        System.out.println(a.name);
+//    }
+//}
+//
+//
+///**
+// * A
+// */
+//public class A {
+//
+//    public static String name="hsedu";
+//
+//
+//}
