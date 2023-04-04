b80799e2257dfbbbfed5189ae2c910f4fa2a58c5
houshuai <m17856943636@163.com>
2023-04-04 08:59:13 +0800

    更新

--- javaAdvanced/javaAdvanced/src/com.hsedu.single_/SingleTon01.java
+++ javaAdvanced/javaAdvanced/src/com.hsedu.single_/SingleTon01.java
@@ -5,6 +5,8 @@ public class SingleTon01 {
         // GirlFirend xh=new
         GirlFirend girlFirend = GirlFirend.getInstance();
         System.out.println(girlFirend);
+
+
     }
 }
 
@@ -12,20 +14,24 @@ class GirlFirend {
     private String name;
 
     /**
-     * 单例模式
+     * 单例模式饿汉式
      * 1、私有化构造器
-     * 2、在类中创建对象
-     * 3、getInstance
+     * 2、在类中创建对象static
+     * 3、向外暴露一个公共方法getInstance
      */
 
+    // 2、 创建一个对象
     private static GirlFirend gf = new GirlFirend("gong");
 
+    // 1、私有化构造器
     private GirlFirend(String name) {
         this.name = name;
     }
 
+    // 3、暴露一个公共方法
     public static GirlFirend getInstance() {
         return gf;
 
     }
 }
+
