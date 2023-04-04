45990567eec4edaad9b611064299d65ab11edf8e
houshuai <m17856943636@163.com>
2023-04-04 08:57:18 +0800

    更新

--- javaAdvanced/javaAdvanced/src/com.hsedu.single_/CatS.java
+++ javaAdvanced/javaAdvanced/src/com.hsedu.single_/CatS.java
@@ -1,2 +1,26 @@
-package com.hsedu.single_;public class CatS {
+package com.hsedu.single_;
+
+/**
+ * 单例模式懒汉式
+ */
+public class CatS {
+    private String name;
+    private static CatS cat;// 未调用构造器
+    private static int n1 = 100;//
+
+    private CatS(String name) {
+        System.out.println("构造器输出");
+        this.name = name;
+    }
+
+    public static CatS getInstance() {
+        if (cat == null) {
+            cat = new CatS("moo");// 创建对象
+        }
+        return cat;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- javaAdvanced/javaAdvanced/src/com.hsedu.single_/SingleTon02.java
+++ javaAdvanced/javaAdvanced/src/com.hsedu.single_/SingleTon02.java
@@ -2,31 +2,7 @@ package com.hsedu.single_;
 
 public class SingleTon02 {
     public static void main(String[] args) {
-        Cat instacnce = Cat.getInstance();
+        CatS instacnce = CatS.getInstance();
     }
 }
 
-/**
- * 单例模式懒汉式
- */
-public class Cat {
-    private String name;
-    private static Cat cat;// 未调用构造器
-    private static int n1 = 100;//
-
-    private Cat(String name) {
-        System.out.println("构造器输出");
-        this.name = name;
-    }
-
-    public static Cat getInstance() {
-        if (cat == null) {
-            cat = new Cat("moo");// 创建对象
-        }
-        return cat;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-}
