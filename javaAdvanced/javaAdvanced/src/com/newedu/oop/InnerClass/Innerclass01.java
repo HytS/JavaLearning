785720dbf2786cf64a1facf0a9add2ce48269aca
houshuai <m17856943636@163.com>
2023-11-14 16:50:59 +0800

    更新

--- javaAdvanced/javaAdvanced/src/com/newedu/oop/InnerClass/Innerclass01.java
+++ javaAdvanced/javaAdvanced/src/com/newedu/oop/InnerClass/Innerclass01.java
@@ -1,2 +1,25 @@
-package com.newedu.oop.InnerClass;public class Innerclass01 {
+package com.newedu.oop.InnerClass;
+
+public class Innerclass01 {
+    public static void main(String[] args) {
+        //匿名内部类的实现
+        new X().fun1();
+//        System.out.println();
+    }
 }
+interface A{
+    public void printInfo() ;	//
+}
+class B implements A{	// 实现接口
+    public void printInfo(){
+        System.out.println("Hello World!!!") ;
+    }
+};
+class X {
+    public void fun1(){
+        this.fun2(new B()) ;
+    }
+    public void fun2(A a){
+        a.printInfo() ;
+    }
+};
