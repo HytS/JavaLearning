f1282cb30de675e26916047b799f78260b27a66b
houshuai <m17856943636@163.com>
2023-11-14 16:51:05 +0800

    更新

--- javaAdvanced/javaAdvanced/src/com/newedu/oop/InnerClass/Innerclass02.java
+++ javaAdvanced/javaAdvanced/src/com/newedu/oop/InnerClass/Innerclass02.java
@@ -1,2 +1,22 @@
-package com.newedu.oop.InnerClass;public class Innerclass02 {
+package com.newedu.oop.InnerClass;
+
+public class Innerclass02 {
+    public static void main(String[] args) {
+
+    }
+}
+interface A1{
+    void print();
+}
+
+class X1{
+    void fun1(){this.fun2(new A1(){
+        @Override
+        public void print() {
+            System.out.println("hello");
+        }
+    });}
+    void fun2(A1 a1){
+        a1.print();
+    }
 }
