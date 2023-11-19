a7ff801eae12de24bf15cfa2b4817a1422a482da
houshuai <m17856943636@163.com>
2023-11-19 20:59:43 +0800

    更新

---
+++ javaAdvanced/javaAdvanced/src/com/newedu/APP/Genericity/StuData.java
@@ -0,0 +1,14 @@
+package com.newedu.APP.Genericity;
+
+public class StuData implements Data<Student>{
+
+    @Override
+    public void add(Student student) {
+
+    }
+
+    @Override
+    public void remove(Student student) {
+
+    }
+}
